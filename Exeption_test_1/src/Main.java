d262d87042338340558aaad3267de11d468fcc0b
Luna-Schaetzle <Luna.Schaetzle@gmail.com>
2023-03-14 15:51:15 +0100

    Testschreiben

--- Exeption_test_1/src/Main.java
+++ Exeption_test_1/src/Main.java
@@ -1,2 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+        String myString = "Hello World";
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("myFile.txt");
+            writer.write(myString);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Succefully wrote to file.");
+
+
+    }
+
+
 }
