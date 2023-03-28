8a30ac599e2d7932b4ae4bd16c3dad40ecd74c8d
Luna-Schaetzle <Luna.Schaetzle@gmail.com>
2023-03-28 22:18:33 +0200

    Filereader kann Jetzt das gleiche Feile direkt einlesen

--- Spaß_mit_Enums/src/FileWirter.java
+++ Spaß_mit_Enums/src/FileWirter.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -12,9 +14,67 @@ public class FileWirter {
         }
         System.out.println("Success");
 
+        /**
+         * Read the file and print it to the console
+         */
+            readAndPrintFileContents(args[0]);
+
+
+        /*
+        try {
+            readFromFile(args[0]);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
+         */
+
+
     }
 
 
+    /**
+     * Read a file and print it to the console
+     * @param fileName
+     */
+    public static void readAndPrintFileContents(String fileName) {
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try {
+            fr = new FileReader(fileName);
+            br = new BufferedReader(fr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+
+                if (fr != null)
+                    fr.close();
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+
+    /**
+     * Write a string to a file
+     * @param filename
+     * @param contant
+     * @throws IOException
+     */
     private static void writeToFile(String filename,String contant) throws IOException {
         FileWriter writer = null;
         writer = new FileWriter(filename);
