93f842321792bbc53d8758c2cd4c5617ea5bf37f
Luna-Schaetzle <Luna.Schaetzle@gmail.com>
2023-03-31 11:27:36 +0200

    Testet somthing

--- Spaß_mit_Enums/src/cmdtest.java
+++ Spaß_mit_Enums/src/cmdtest.java
@@ -1,11 +1,23 @@
+import java.io.IOException;
+
 public class cmdtest {
 
     public static void main(String[] args) {
 
+        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "dir");
         try {
-            Process rt = Runtime.getRuntime().exec("cmd /c dir");
-        }catch (Exception e) {
-            System.out.println("sorry somthing went wrong");
+            Process p = pb.start();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Success");
+
+
+        try {
+            Runtime.getRuntime().exec("cmd /c cls");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         System.out.println("Success");
 
