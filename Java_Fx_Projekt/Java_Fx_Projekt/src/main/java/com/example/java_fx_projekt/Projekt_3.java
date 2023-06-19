395f15dd5f7e93b8666b4b016d88f7cfb75d825b
Luna-Schaetzle <Luna.Schaetzle@gmail.com>
2023-06-19 15:44:20 +0200

    Projekt ist verbessert Projekt_3

--- Java_Fx_Projekt/Java_Fx_Projekt/src/main/java/com/example/java_fx_projekt/Projekt_3.java
+++ Java_Fx_Projekt/Java_Fx_Projekt/src/main/java/com/example/java_fx_projekt/Projekt_3.java
@@ -4,19 +4,24 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class Projekt_2 extends Application {
+public class Projekt_3 extends Application {
 
     public static void main(String[] args) {
         launch(args);
@@ -27,19 +32,22 @@ public class Projekt_2 extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Projekt_V_1");
+        primaryStage.setTitle("Projekt_V_2");
 
         // Hallo-Text
-        Text hallo = new Text("Hallo!");
-        hallo.setStyle("-fx-font-size: 24;");
+        Text hallo = new Text("</Hallo World!>");
+        hallo.setFont(Font.font(24));
+        hallo.setFill(Color.WHITE);
 
         // Nachricht-Text
         Text nachricht = new Text();
-        nachricht.setStyle("-fx-font-size: 18;");
+        nachricht.setFont(Font.font(18));
+        nachricht.setFill(Color.WHITE);
 
         // Button
         Button button = new Button("Neue Nachricht generieren");
-        button.setStyle("-fx-font-size: 16;");
+        button.setFont(Font.font(16));
+        button.setStyle("-fx-background-color: #4CAF50; -fx-text-fill: white;");
         button.setOnAction(e -> {
             // Nachricht generieren
             String nachricht_String = "Ich ";
@@ -53,7 +61,7 @@ public class Projekt_2 extends Application {
         // VBox für die Anordnung der Elemente
         VBox vbox = new VBox(20, hallo, nachricht, button);
         vbox.setPadding(new Insets(20));
-        vbox.setStyle("-fx-background-color: #f4f4f4;");
+        vbox.setBackground(new Background(new BackgroundFill(Color.rgb(35, 40, 45), CornerRadii.EMPTY, Insets.EMPTY)));
 
         Scene scene = new Scene(vbox, 500, 400);
         primaryStage.setScene(scene);
@@ -62,7 +70,7 @@ public class Projekt_2 extends Application {
 
     // leere Zeilen in den Dateien vermeiden
     static String input_1_auslesen() {
-        List<String> lines = readLinesFromFile(input_1.toString());
+        List<String> lines = readLinesFromFile(input_1);
         if (!lines.isEmpty()) {
             int lineNumber = new Random().nextInt(lines.size());
             return lines.get(lineNumber);
@@ -71,7 +79,7 @@ public class Projekt_2 extends Application {
     }
 
     static String input_2_auslesen() {
-        List<String> lines = readLinesFromFile(input_2.toString());
+        List<String> lines = readLinesFromFile(input_2);
         if (!lines.isEmpty()) {
             int lineNumber = new Random().nextInt(lines.size());
             return lines.get(lineNumber);
@@ -79,18 +87,12 @@ public class Projekt_2 extends Application {
         return "";
     }
 
-    private static List<String> readLinesFromFile(String filePath) {
-        List<String> lines = new ArrayList<>();
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = bufferedReader.readLine()) != null) {
-                if (!line.trim().isEmpty()) {
-                    lines.add(line);
-                }
-            }
+    private static List<String> readLinesFromFile(Path filePath) {
+        try {
+            return Files.readAllLines(filePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return lines;
+        return List.of();
     }
 }
